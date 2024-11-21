3959d88dc63239d858b4c440deb13279efd55dbd
aarekr <aare212@hotmail.com>
2024-11-21 21:07:05 +0200

    OpenAI chat bot

---
+++ chatbot/app.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template, request, jsonify
+import openai
+from os import getenv
+
+app = Flask(__name__)
+openai.api_key  = getenv("OPENAI_API_KEY")
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+@app.route("/")
+def home():    
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():    
+    userText = request.args.get('msg')  
+    response = get_completion(userText)  
+    #return str(bot.get_response(userText)) 
+    return response
+
+if __name__ == "__main__":
+    app.run()
